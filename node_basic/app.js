584001e19a231d90f98d7d8a371598bb96e173f7
gangadhar222 <gangadhar.eee@hotmail.com>
2021-04-24 15:24:14 +0530

    simple server using http

--- node_basic/app.js
+++ node_basic/app.js
@@ -2,7 +2,18 @@
 // const helpers = require('./helpers')
 
 // the other way
-const {sum} = require('./helpers');
+const { sum } = require("./helpers");
 
-const total = sum(2,3)
-console.log(total)
+// core module of node
+const http = require("http");
+
+// create server is a method to create a server using http
+const server = http.createServer((req, res) => {
+  res.end("Created simple server using node");
+});
+
+// listenig to server
+server.listen(3001);
+
+const total = sum(2, 3);
+console.log(total);
